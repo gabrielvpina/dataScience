334e360155333eac8a47599f3efad3d1b85efbad
gabrielvpina <gvpina.rodrigues@gmail.com>
2025-11-27 15:41:46 -0300

    notas parciais

--- machine_learning/linear_regression/lm.R
+++ machine_learning/linear_regression/lm.R
@@ -1,11 +1,11 @@
-library(dplyr)
-
+# importando dados
 data <- mtcars
 
 # correlação simples
 cor(data)
 
 # correlação plot
+# install.packages("GGally")
 library(GGally)
 ggpairs(data, ggplot2::aes(fill="#3d85bc", alpha=0.6)) 
 
@@ -31,3 +31,28 @@ ggplot(data, aes(x=disp, y=wt)) +
   geom_point(size=4, alpha=0.7, color="#3d85bc") +
   geom_smooth(method = "lm") +
   theme_minimal()
+
+
+
+
+# ====================================================================================
+# DADOS DA SALA
+
+myDf <- read.csv("data/dataset_gemini.csv")
+
+cor(myDf[,c(2:4)])
+
+ggplot(myDf, aes(x=altura,y=numero_pe)) +
+  geom_point(size = 4, alpha=0.7, color="darkgreen") +
+  theme_classic()
+
+# modelo linear
+modelo <- lm(data=myDf, formula=numero_pe ~ altura)
+summary(modelo)
+modelo
+
+
+ggplot(myDf, aes(x=altura,y=numero_pe)) +
+  geom_point(size = 4, alpha=0.7, color="darkgreen") +
+  theme_classic() +
+  geom_smooth(method = "lm")
---
+++ notas_seminario.md
@@ -0,0 +1,60 @@
+# Requisitos
+- Histórico e Conceito (1)
+- Pontos Positivos (2)
+- Limitações (3)
+- Principais utilizações (4)
+- Exemplo de Uso (5)
+
+# David e Luciana: SVM
+
+1 (ok) | 2 (ok) | 3 (ok) | 4 (ok) | 5 (ok)
+
+- Nota Gabriel: 10
+- Nota Eric: 9.2
+
+- Nota final: 9.6
+
+# Mychael e Gabriel Carvalho: Random Forest
+
+1 (ok) | 2 (ok) | 3 (ok) | 4 (ok) | 5 (ok)
+
+- Nota Gabriel: 9.0
+- Nota Eric: 8.5
+
+- Nota final: 8.75
+
+# Luiz e Marcos: XGBoost
+
+1 (ok) | 2 (ok) | 3 (ok) | 4 (ok) | 5 (ok)
+
+- Nota Gabriel: 9.3
+- Nota Eric: 9.4
+
+- Nota final:
+
+## Anderson e Gabriel: Regressão Polinomial
+
+1 () | 2 () | 3 () | 4 () | 5 ()
+
+- Nota Gabriel:
+- Nota Eric: 8
+
+- Nota final:
+
+## Mateus e israel: Naive Bayes
+
+1 () | 2 () | 3 () | 4 () | 5 ()
+
+- Nota Gabriel:
+- Nota Eric:
+
+- Nota final:
+
+## Maria e Laiz: KNN
+
+1 () | 2 () | 3 () | 4 () | 5 ()
+
+- Nota Gabriel:
+- Nota Eric:
+
+- Nota final:
